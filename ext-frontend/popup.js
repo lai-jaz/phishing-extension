492cbd4bcf6ca1aea5fe17fbbabe706cd357ebeb
Fatima Khan <163330776+fatimaKhan003@users.noreply.github.com>
2025-11-22 03:33:25 +0500

    updated popup.js. to correctly display the required details

--- ext-frontend/popup.js
+++ ext-frontend/popup.js
@@ -1,6 +1,10 @@
 document.getElementById("sendBtn").addEventListener("click", async () => {
+    chrome.tabs.query({active:true,currentWindow:true},(tabs)=>
+    {
+        const url=tabs[0].url;
+        chrome.runtime.sendMessage({type: "MANUAL_SCAN", url:url})
+    })
     const name = document.getElementById("nameInput").value;
-
     document.getElementById("result").textContent = name;
     
     // const res = await fetch("http://localhost:3000/api/hello", {
@@ -14,7 +18,6 @@ document.getElementById("sendBtn").addEventListener("click", async () => {
     // const data = await res.json();
     // document.getElementById("result").textContent = data.message;
 });
-
 const dropdownBtn = document.querySelector('.dropdown-btn');
 const logs = document.querySelector('.logs');
 const arrow = dropdownBtn.querySelector('.arrow');
@@ -25,6 +28,27 @@ dropdownBtn.addEventListener('click', () => {
         arrow.style.transform = 'rotate(-180deg)';
     } else {
         arrow.style.transform = 'rotate(0deg)';
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    chrome.runtime.sendMessage({ type: "GET_SCAN_RESULT" }, (response) => {
+        if (!response) return;
+
+        const { url, score, status} = response;
+
+        document.getElementById("result").innerHTML = 
+        `<strong>URL:</strong> ${url}<br> 
+         <strong>Score:</strong> ${score}/100<br>
+         <strong>Status:</strong> ${status}<br>`;
+    });
+});
+chrome.runtime.onMessage.addListener((message) => {
+    if (message.type === "SCAN_RESULT") {
+        const { url, score, status} = message.data;
+        document.getElementById("result").innerHTML = 
+        `<strong>URL:</strong> ${url}<br>
+         <strong>Score:</strong> ${score}/100<br>
+         <strong>Status:</strong> ${status}<br>`;
     }
 });
 
